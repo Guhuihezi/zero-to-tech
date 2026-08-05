39cda3b97b6130e3b42a680a20ad1460153a1adb
XinYu Tong <yu131009@163.com>
2026-08-05 18:39:34 +0800

    修复cards.js

--- js/cards.js
+++ js/cards.js
@@ -1,4 +1,4 @@
-import {animate,stagger} from "https://cdn.jsdelivr.net/npm/animejs@4/lib/anime.iife.min.js"
+import { animate, stagger } from "https://cdn.jsdelivr.net/npm/animejs@4/+esm";
 
 export function initCardsAnim() {
   anime.animate(".card", {
